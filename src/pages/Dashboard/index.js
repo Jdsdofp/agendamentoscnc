bda90141ba9ced36cd56b630f09a285b30b8db6a
unknown <jdsdofp@gmail.com>
2021-10-28 20:13:54 -0300

    console.log no code

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -21,7 +21,7 @@ export default function Dashboard(){
        socket.on('booking_request', data=>{
            setRequests([ ...requests, data ])
 
-           console.log(requests)
+           console.log(data)
        })
     }, [requests, socket]);
     
