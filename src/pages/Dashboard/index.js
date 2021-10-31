632340f7448da03f8e8652ae67c27f7573297e31
unknown <jdsdofp@gmail.com>
2021-10-30 21:58:12 -0300

    Alteracao no link

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react";
 import {Link} from 'react-router-dom';
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 import api from '../../services/api';
 import './style.css'
 
