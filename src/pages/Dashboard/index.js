9af3ee5ed8044dc291f6b8ff9d8c744ae7153b86
unknown <jdsdofp@gmail.com>
2021-11-05 22:13:24 -0300

    Inclusão do console log para verificação

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -30,6 +30,7 @@ export default function Dashboard(){
                 headers: { user_id }
             });
             setSpots(response.data)
+            console.log(response.data)
         }
         loadSpots();
     }, [])
